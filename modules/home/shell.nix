0d699d901841aea554849955594a073810c491e6
Jonáš Malena <jonas.malena@gmail.com>
2026-07-09 11:43:32 +0200

    feat(shell): report cwd to apple terminal so new tabs open in the same folder

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -6,6 +6,27 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
+    # Report the working directory to Apple Terminal so new tabs/windows
+    # (Cmd+T / Cmd+N) open in the current folder.
+    initContent = ''
+      if [ "$TERM_PROGRAM" = "Apple_Terminal" ]; then
+        autoload -Uz add-zsh-hook
+        _osc7_cwd() {
+          local url="file://$HOST" i ch
+          for ((i = 1; i <= ''${#PWD}; i++)); do
+            ch="$PWD[i]"
+            case "$ch" in
+              [a-zA-Z0-9/._~-]) url+="$ch" ;;
+              *) url+=$(printf '%%%02X' "'$ch") ;;
+            esac
+          done
+          printf '\e]7;%s\a' "$url"
+        }
+        add-zsh-hook chpwd _osc7_cwd
+        _osc7_cwd
+      fi
+    '';
+
     history = {
       size = 100000;
       save = 100000;
